510ce1d5c7f0213f9d154a85a71e1a16dcaf559d
Matthew Finerty <evilarceus@gmail.com>
2017-06-12 21:47:29 -0700

    Added more comments

--- Music.js
+++ Music.js
@@ -15,14 +15,20 @@ module.exports = {
             }
         }
 
+        /* !m_play command */
         if (msg.content.startsWith("!m_play")) {
+            // Get user-specified URL
             var url = msg.content.substring(8);
+
+            // In the case there's a parameter in the URL
             if (url.includes("&")) {
                 url.substring(0, url.indexOf("&"))
             }
 
+            // Get voice channel to join
             const voiceChannel = msg.member.voiceChannel;
 
+            // Return if the user is not in a voice channel
             if (!voiceChannel) {
                 return msg.reply("Please be in a valid voice channel.");
             }
@@ -31,17 +37,22 @@ module.exports = {
 
             youtubedl.getInfo(url, function (err, info) {
                 if (err) {
+                    // Invalid or unsupported link
                     return msg.reply("Please enter a valid link")
                 }
                 else {
+                    // Get audio stream information using node-youtube-dl
                     var stream = youtubedl(url);
 
+                    // Wait until stream loads
                     stream.on('info', function (info) {
+                        // Join voice channel
                         voiceChannel.join()
                             .then(connection => {
                                 msg.reply("Now playing: **" + info.title + "** in voice channel **" +
                                     msg.member.voiceChannel.name + "**.");
 
+                                // Play audio in voice channel and all that stuff
                                 dispatcher = connection.playStream(stream);
                                 dispatcher.setVolume(Config.defaultVolume);
                                 dispatcher.on('end', () => {
@@ -53,7 +64,9 @@ module.exports = {
             });
         }
 
+        /* !m_pause command */
         if (msg.content.startsWith("!m_pause")) {
+            // Return if there's no audio playing
             if (dispatcher === undefined) {
                 return msg.reply("No audio is currently playing.")
             }
@@ -62,7 +75,9 @@ module.exports = {
             dispatcher.pause();
         }
 
+        /* !m_resume command */
         if (msg.content.startsWith("!m_resume")) {
+            // Return if there's no audio playing
             if (dispatcher === undefined) {
                 return msg.reply("No audio is currently playing.")
             }
@@ -71,7 +86,9 @@ module.exports = {
             dispatcher.resume();
         }
 
+        /* !m_stop command */
         if (msg.content.startsWith("!m_stop")) {
+            // Return if there's no audio playing
             if (dispatcher === undefined) {
                 return msg.reply("No audio is currently playing.")
             }
@@ -80,35 +97,44 @@ module.exports = {
             dispatcher.end();
         }
 
+        /* !m_volume command */
         if (msg.content.startsWith("!m_volume")) {
+            // Return if there's no audio playing
             if (dispatcher === undefined) {
                 return msg.reply("No audio is currently playing.")
             }
 
+            // Set audio to default
             if (msg.content.includes("default")) {
                 dispatcher.setVolume(Config.defaultVolume);
                 return msg.reply("Setting volume to default (" + Config.defaultVolume * 100 + "%)...")
             }
 
+            // Parse to integer/float depending on what the user inputted
             if (msg.content.includes(".")) {
                 var volume = parseFloat(msg.content.substring(10))
             } else var volume = parseInt(msg.content.substring(10));
 
+            // Return if the number is negative
             if (volume < 0) {
                 return msg.reply("Negative volume? I don't know how to do that.");
             }
 
+            // Return if the number is not actually a number ( ͡° ͜ʖ ͡°)
             if (isNaN(volume)) {
                 return msg.reply("Please enter a numerical value.")
             }
 
+            // Set playback volume
             msg.reply("Setting volume of audio playback to " + volume * 100 + "%...");
             dispatcher.setVolume(volume);
         }
 
+        /* !m_volume command */
         if (msg.content.startsWith("!m_whitelist")) {
             var tchannel = msg.content.substring(13);
 
+            // Give user command reference if an argument wasn't given
             if (tchannel === "" || tchannel === null) {
                 var _whitelistChannel = Config.musicChannel;
                 if (_whitelistChannel === undefined) { _whitelistChannel = "ALL" }
@@ -116,16 +142,19 @@ module.exports = {
                 return msg.reply("```!m_whitelist - Set music channel to whitelist. Current: " + _whitelistChannel + "```");
             }
 
+            // Set whitelisted channel to none (disable music commands)
             if (tchannel === "NONE") {
                 Config.musicChannel = null;
                 return msg.reply("Setting music command whitelist to NONE.");
             }
 
+            // Set whitelisted channel to all (enable music commands across all channels)
             if (tchannel === "ALL") {
                 Config.musicChannel = undefined;
                 return(msg.reply("Setting music command whitelist to ALL channels."));
             }
 
+            // Set whitelisted channel to what the user specifies
             else {
                 Config.musicChannel = tchannel;
                 return msg.reply("Setting music command whitelist to #" + tchannel + ".");
