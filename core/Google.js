16f8f50f3d40d1c426cfd6b474145fbfdc9da4d8
evilarceus <evilarceus@gmail.com>
2017-12-10 19:59:27 -0800

    Added an exception in case CSE vars are undefined

--- core/Google.js
+++ core/Google.js
@@ -1,9 +1,13 @@
 const config = require("../config");
 const googleImages = require("google-images");
-const client = new googleImages(config.googleCSE, config.googleCSE_APIKey);
+var client;
+if (config.googleCSE && config.googleCSE_APIKey) {
+    client = new googleImages(config.googleCSE, config.googleCSE_APIKey);
+}
 
 module.exports = {
     imageHandler: function(msg) {
+        if (!client) return msg.reply("Google Custom Search is not set up properly.");
         const query = msg.content.substring(7);
         // ignore this lol
         if (msg.author.username === "WillyJ") { return msg.reply("No.") }
